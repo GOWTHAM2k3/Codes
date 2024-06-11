ea47074d95cdee5ff3035eb466ab48fe0fc2e1e2
GOWTHAM2k3 <gowthamslsg@gmail.com>
2024-06-11 21:16:43 +0530

    Add files via upload

--- struct.c
+++ struct.c
@@ -1,34 +1,54 @@
-#include<stdio.h>
-
-struct employee{
-	int id;
-	float salary;
-	char dept;
-};
-
-void main() 
-{
-	struct employee e1 = {111, 25000.25, 'C'};
-	struct employee e2 = {112, 30000.56, 'E'};
-	struct employee e3;
-	printf("ID of E1: %d",e1.id);
-	printf("ID of E2: %d",e2.id);
-	printf("Dept of E1: %c\n",e1.id);
-	printf("Dept of E2: %c\n",e2.id);
-	printf("Dept of E1: %c\n",e1.dept);
-	printf("Dept of E2: %c\n",e2.dept);
-	printf("After manipulation of data\n");
-	e1.dept = 'X';
-	e2.dept = 'y';
-	printf("Dept of E1: %c\n",e1.id);
-	printf("Dept of E2: %c\n",e2.id);
-	printf("Dept of E1: %c\n",e1.dept);
-	printf("Dept of E2: %c\n",e2.dept);
-	
-	printf("starting for E3\n");
-	e3.id=115;
-	e3.salary=46000;
-	e3.dept='W';
-	printf("E3 salary: %f",e3.salary);
-	
+public class CountNodes {    
+    class Node{    
+        int data;    
+        Node next;    
+        public Node(int data) {    
+            this.data = data;    
+            this.next = null;    
+        }    
+    }       
+    public Node head = null;    
+    public Node tail = null;    
+    public void addNode(int data) {    
+        Node newNode = new Node(data);    
+        if(head == null) {    
+            head = newNode;    
+            tail = newNode;    
+        }    
+        else {    
+            tail.next = newNode;    
+            tail = newNode;    
+        }    
+    }    
+    public int countNodes() {    
+        int count = 0;    
+        Node current = head;    
+        while(current != null) {    
+            count++;    
+            current = current.next;    
+        }    
+        return count;    
+    }    
+    public void display() {    
+        Node current = head;    
+        if(head == null) {    
+            System.out.println("List is empty");    
+            return;    
+        }    
+        System.out.println("Nodes of singly linked list: ");    
+        while(current != null) {    
+            System.out.print(current.data + " ");    
+            current = current.next;    
+        }    
+        System.out.println();    
+    }    
+    public static void main(String[] args) {    
+        CountNodes sList = new CountNodes();    
+        sList.addNode(1);    
+        sList.addNode(2);    
+        sList.addNode(3);    
+        sList.addNode(4);    
+        sList.display();    
+        System.out.println("Count of nodes present in the list: " + sList.countNodes());    
+    }    
 }
